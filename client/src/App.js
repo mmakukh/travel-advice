be1284a37fea5d38dd528646c901fc33d905b43d
Mariya Makukh <marymakukh@gmail.com>
2022-06-16 22:50:36 -0700

    draft login and register

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './Header.js';
-import Footer from './Footer.js';
-import Body from './Body.js';
-
+import "./App.css";
+import Homepage from "./Homepage/Homepage";
+import Login from "./Login/Login";
+import Register from "./Register/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 function App() {
+  const [user, setLoginUser] = useState({});
   return (
     <div className="App">
-      <header className="App-header">
-        <Header />
-      </header>
-      <body> 
-        <Body />
-      </body>
-      < footer>
-        <Footer />
-      </footer>
+      <Router>
+        <Routes>
+          <Route exact path="/">
+            {user && user._id ? <Homepage /> : <Login />}
+            <Homepage />
+          </Route>
+          <Route path="/Login">
+            <Login setLoginUser={setLoginUser} />
+          </Route>
+          <Route path="/Register">
+            <Register />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
