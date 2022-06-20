d83a90d99d3c6cc96012f76820c306abac89f837
Mariya Makukh <marymakukh@gmail.com>
2022-06-19 18:33:14 -0700

    Add API to get list of Items

---
+++ server/routes/travels.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const recordRoutes = express.Router();
+const dbo = require("../db/conn");
+const ObjectId = require("mongodb").ObjectId;
+
+recordRoutes.route("/travel").get(function (req, res) {
+  let db_connect = dbo.getDb("travel-advice");
+  db_connect
+    .collection("items")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+recordRoutes.route("/travel/:id").get(function (req, res) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("items").findOne(myquery, function (err, result) {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+module.exports = recordRoutes;
